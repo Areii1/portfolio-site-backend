0b8abc58fd4d9f984056f58dd70e5200ae14d080
ari-pekka jääskeläinen <ari.jaaskelainen1@gmail.com>
2020-07-19 23:57:11 +0300

    add query validation

--- handler.ts
+++ handler.ts
@@ -1,12 +1,55 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
+import { APIGatewayProxyHandler } from "aws-lambda";
+import "source-map-support/register";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
-}
+const validateQueryData = (data: any) => {
+  const keys = Object.keys(data);
+  console.log(keys, "keys");
+  if (keys.length > 4) {
+    return "provide no more than four arguments";
+  }
+  const hasName = keys.some((key) => key === "name");
+  console.log(hasName, "hasName");
+  if (!hasName) {
+    return "name field missing";
+  }
+  const hasEmail = keys.some((key) => key === "email");
+  console.log(hasEmail, "hasEmail");
+  if (!hasEmail) {
+    return "email field missing";
+  }
+  const hasSubject = keys.some((key) => key === "subject");
+  console.log(hasSubject, "hasSubject");
+  if (!hasSubject) {
+    return "subject field missing";
+  }
+  const hasMessage = keys.some((key) => key === "message");
+  console.log(hasMessage, "hasMessage");
+  if (!hasMessage) {
+    return "message field missing";
+  }
+};
+
+type ValidatedData = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+export const logInfo: APIGatewayProxyHandler = async (event, _context) => {
+  const data = JSON.parse(event.body);
+  const validationResponse = validateQueryData(data);
+  if (!validationResponse) {
+    const validatedData: ValidatedData = data;
+    console.log(validatedData, "validatedData");
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ response: "received query in correct form" }),
+    };
+  } else {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: validationResponse }),
+    };
+  }
+};
